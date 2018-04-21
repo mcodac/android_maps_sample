f32d9e8ab3f6bcbf1cdf1dd317a07287bde66fb5
moises <moises.coda@gmail.com>
2018-04-21 02:33:28 +0200

    Changes made to complete exercise

--- app/src/main/java/edu/uoc/android/contacts/ui/SplashActivity.java
+++ app/src/main/java/edu/uoc/android/contacts/ui/SplashActivity.java
@@ -1,10 +1,13 @@
 package edu.uoc.android.contacts.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+
+import edu.uoc.android.contacts.ContactMapActivity;
 import edu.uoc.android.contacts.R;
 import edu.uoc.android.contacts.manager.FirebaseContactManager;
 import edu.uoc.android.contacts.model.Contact;
@@ -38,5 +41,8 @@ public class SplashActivity extends AppCompatActivity implements ValueEventListe
 
     private void startMainActivity() {
         // TODO: make an Intent to go to contact list activity
+        Intent intent = new Intent(this, ContactMapActivity.class);
+        startActivity(intent);
+        this.finish();
     }
 }
